5c1f43c493086ceb06650a50d4c2b3a4ea2a30f9
Nathan Patterson <nathan.e.patterson@gmail.com>
2025-02-19 09:50:20 -0700

    Make page a server component

--- src/app/(portal)/page.tsx
+++ src/app/(portal)/page.tsx
@@ -1,36 +1,4 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { LoadingSpinner } from "@/components/global/loading-spinner";
-import { useUserRole } from "@/hooks/useUserRole";
-
+import PortalRedirect from "@/components/global/portal-redirect";
 export default function PortalPage() {
-  const router = useRouter();
-
-  const { role, isAuthenticated } = useUserRole();
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (!isAuthenticated) return;
-
-    console.log("Role Inside of base page", role);
-    if (role === "admin") {
-      router.replace("/admin");
-    } else {
-      router.replace("/dashboard");
-    }
-
-    setLoading(false);
-  }, [router, role, isAuthenticated]);
-
-  if (loading)
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <LoadingSpinner />
-      </div>
-    );
-
-  return null; // Redirecting, so nothing is rendered
+  return <PortalRedirect />;
 }
